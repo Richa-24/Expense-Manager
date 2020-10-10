62f11343dee0fa5cb5488aed9ede254e0eeec7bb
Richa Singh <singhricha0724@gmail.com>
2020-10-10 15:51:26 +0530

    React app initialization

---
+++ react/src/Components/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    render() {
+        const { email, password } = this.state
+        return (
+            <>
+                <h1>Login</h1>
+                <input type="email" value={email} name="" onChange={} />
+                <input type="password" value={password} name="" onChange={} />
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
